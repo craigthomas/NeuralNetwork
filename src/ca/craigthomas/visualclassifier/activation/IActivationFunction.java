6f458ee30317b89dc39cfe2a591f7fb24d42ff0f
Craig Thomas <craig.thomas@gmail.com>
2014-09-07 13:17:23 -0700

    Update header and description of interface.

--- src/ca/craigthomas/visualclassifier/activation/IActivationFunction.java
+++ src/ca/craigthomas/visualclassifier/activation/IActivationFunction.java
@@ -1,7 +1,19 @@
+/*
+ * Copyright (C) 2014 Craig Thomas
+ * This project uses an MIT style license - see LICENSE for details.
+ */
 package ca.craigthomas.visualclassifier.activation;
 
 import org.jblas.DoubleMatrix;
 
+/**
+ * An interface to capture an activation function. There are several different
+ * activation functions that one might wish to use within a neural network. At
+ * a minimum, each activation function can be applied to a particular matrix,
+ * as well as return the gradient for that function along a given input.
+ * 
+ * @author thomas
+ */
 public interface IActivationFunction {
 
     public DoubleMatrix apply(DoubleMatrix input);
